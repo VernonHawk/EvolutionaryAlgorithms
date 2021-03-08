d56adcf667de89541acdaa7556f10b7219ca6e0b
Ihor Morenets <i@morenets.dev>
2021-03-08 21:55:37 +0200

    Implement test functions

--- src/testFunctions.ts
+++ src/testFunctions.ts
@@ -2,22 +2,30 @@ import {Individual} from './common'
 
 export type TestFunction = (individual: Individual) => number
 
-export const deba1: TestFunction = (individual) => {
-  // TODO
-  return 0
-}
+export const deba1: TestFunction = individual =>
+  individual.reduce((acc, curr) => acc + Math.pow(Math.sin(5 * Math.PI * curr), 6), 0) /
+  individual.length
 
-export const deba2: TestFunction = (individual) => {
-  // TODO
-  return 0
-}
+export const deba2: TestFunction = individual =>
+  individual.reduce(
+    (acc, curr) =>
+      acc +
+      Math.pow(Math.E, -2 * Math.log(2) * Math.pow((curr - 0.1) / 0.8, 2)) *
+        Math.pow(Math.sin(5 * Math.PI * curr), 6),
+    0,
+  ) / individual.length
 
-export const deba3: TestFunction = (individual) => {
-  // TODO
-  return 0
-}
+export const deba3: TestFunction = individual =>
+  individual.reduce(
+    (acc, curr) => acc + Math.pow(Math.sin(5 * Math.PI * (Math.pow(curr, 0.75) - 0.05)), 6),
+    0,
+  ) / individual.length
 
-export const deba4: TestFunction = (individual) => {
-  // TODO
-  return 0
-}
+export const deba4: TestFunction = individual =>
+  individual.reduce(
+    (acc, curr) =>
+      acc +
+      Math.pow(Math.E, -2 * Math.log(2) * Math.pow((curr - 0.08) / 0.854, 2)) *
+        Math.pow(Math.sin(5 * Math.PI * (Math.pow(curr, 0.75) - 0.05)), 6),
+    0,
+  ) / individual.length
